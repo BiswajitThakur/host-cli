73215d5c3e42bde47640a5519a4785439defbf4d
BiswajitThakur <biswajit733127@gmail.com>
2023-08-09 09:46:16 +0530

    test

--- fn_test.go
+++ fn_test.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+    "os"
+    "reflect"
     "testing"
 )
 
-func TestIsExist(t *testing.T){
+var test_file string = "test.file.txt"
+
+func TestIsExist(t *testing.T) {
 
     // any path that exists
     var got bool = IsExist("fn_test.go")
@@ -20,7 +24,7 @@ func TestIsExist(t *testing.T){
     }
 }
 
-func TestIsExistOneOfMany(t *testing.T){
+func TestIsExistOneOfMany(t *testing.T) {
     var got bool = IsExistOneOfMany("path1", "fn_test.go", "path2")
     var want bool = true
     if got != want {
@@ -35,7 +39,7 @@ func TestIsExistOneOfMany(t *testing.T){
     }
 }
 
-func TestReadFile(t *testing.T){
+func TestReadFile(t *testing.T) {
 
     var _, got = ReadFile("fn_test.go")
     var want error = nil
@@ -53,7 +57,7 @@ type isCmt struct {
     input string
     want bool
 }
-func TestIsComment(t *testing.T){
+func TestIsComment(t *testing.T) {
     var got_want []isCmt = []isCmt{
         isCmt{"val", false},
         isCmt{"   5feAvZ  ", false},
@@ -80,7 +84,7 @@ func TestIsComment(t *testing.T){
     }
 }
 
-func TestFindStr(t *testing.T){
+func TestFindStr(t *testing.T) {
     var s1 = []string{"abc", "bc", "ca", "ac"}
     got_want := [][]int{
         { FindStr(s1, "ca"), 2},
@@ -98,3 +102,112 @@ func TestFindStr(t *testing.T){
         }
     }
 }
+
+func TestWriteFile(t *testing.T) {
+    var got error = WriteFile(test_file, "*Hello World*")
+    var want error = nil
+    if got != want {
+        t.Errorf("got: %v, want: %v\n", got, want)
+    }
+
+    var got1 bool = IsExist(test_file)
+    var want1 bool = true
+    if got1 != want1 {
+        t.Errorf("got: %v, want: %v\n", got1, want1)
+    }
+
+    got2, _ := ReadFile(test_file)
+    want2 := "*Hello World*"
+    if got2 != want2 {
+        t.Errorf("got: %v, want: %v\n", got2, want2)
+    }
+    _ = os.Remove(test_file)
+}
+
+
+func TestFilterHosts(t *testing.T){
+    var h string = `# this is comment
+0.0.0.0  fff.com
+  # 0.0.0.0 abc.in
+127.0.0.1 bt.com
+
+#127.0.0.1  hello.com
+
+   
+localhost  kk.in.com
+##  ###
+123.36.0.44       ee.edu
+
+
+localhost  foo.com
+ddd.com  iii.in`
+
+    var got []string = FilterHosts(h)
+    var want []string = []string{
+        "fff.com",
+        "bt.com",
+        "kk.in.com",
+        "ee.edu",
+        "foo.com",
+   //   "iii.in"
+    }
+    if !reflect.DeepEqual(got, want) {
+        t.Errorf("got: %v, want: %v\n", got, want)
+    }
+}
+
+
+func TestFilterRedirectHosts(t *testing.T) {
+    var input string = `# Hello
+aa.com  eq.com
+
+  yu.in   ff.oo
+# ss.com  qwh.com
+#####################
+ghgh.in   kkllu.gg
+# #####
+##### #####
+    #   eeuu.in   vvvkkk.com
+google.com    facebook.com
+`
+    var got [][2]string = FilterRedirectHosts(input)
+    var want [][2]string = [][2]string{
+    	[2]string{"aa.com", "eq.com"},
+    	[2]string{"yu.in", "ff.oo"},
+    	[2]string{"ghgh.in", "kkllu.gg"},
+    	[2]string{"google.com", "facebook.com"},
+    }
+    if !reflect.DeepEqual(got, want) {
+        t.Errorf("got: %v, want: %v\n", got, want)
+    }
+}
+
+func TestReadLocalHosts(t *testing.T) {
+    input := `127.0.0.1   localhost
+###########
+0.0.0.0   kkk.com
+# BT-start #
+0.0.0.0        yooo.in
+127.0.0.1   fooooo.com
+#  127.0.0.1   example.com
+###   ####
+
+123.66.40.222  google.com
+
+# BT-end  #
+127.0.0.1   ioio.io
+0.0.0.0   kkk.com
+`
+    _ = WriteFile(test_file, input)
+    var got []string = ReadLocalHosts(test_file)
+    var want []string = []string{
+        "yooo.in",
+        "fooooo.com",
+        "google.com",
+    }
+    if !reflect.DeepEqual(got, want) {
+        t.Errorf("got: %v, want: %v\n", got, want)
+    }
+    _ = os.Remove(test_file)
+}
+
